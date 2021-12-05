779d6fc7c5d1493b0a228e22c5fc93e14b5d4e89
王冬 <wangdong@wangdongdeMacBook-Air.local>
2021-12-05 23:06:58 +0800

    MOD 调整

--- src/App.vue
+++ src/App.vue
@@ -11,17 +11,19 @@ export default {
   data() {
     return {};
   },
+  computed: {},
   async created() {
     if (!window.navigator.onLine) Message.error("网络链接失败, 请检查网络连接");
     let token = this.$store.getters.getToken || "";
+    console.log(token);
     if (token) {
-      let data = await getUserInfo({ token });
-      if (data.code === "0") {
-        this.$store.commit("setUserName", data.data.user);
-        this.$store.commit("setPhoneNumber", data.data.phone);
-        this.$store.commit("setAvatar", data.data.head);
+      let res = await getUserInfo({ token });
+      if (res.code === "0") {
+        this.$store.commit("setUserName", res.data.user);
+        this.$store.commit("setPhoneNumber", res.data.phone);
+        this.$store.commit("setAvatar", res.data.head);
       } else {
-        Message.error(data.msg || `${data.name}: ${data.message}`);
+        Message.error(res.msg || `${res.name}: ${res.message}`);
         localStorage.removeItem("token");
         this.$store.commit("setToken", "");
         this.$router.replace({ path: "/login" });
