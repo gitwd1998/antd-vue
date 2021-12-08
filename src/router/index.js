e395e72df50e9a9744cf27c440dce449cebf42e4
王冬 <wangdong@wangdongdeMacBook-Air.local>
2021-12-08 13:32:36 +0800

    MOD 路由调整

--- src/router/index.js
+++ src/router/index.js
@@ -4,27 +4,36 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [
-  { path: '/', redirect: '/layout' },
-  { path: '/login', component: () => import('@/views/Login') },
-  { path: '/regist', component: () => import('@/views/Regist') },
+  { path: '/', name: 'root', redirect: '/layout' },
+  { path: '/login', name: 'login', component: () => import('@/views/Login') },
+  { path: '/regist', name: 'regist', component: () => import('@/views/Regist') },
   {
-    path: '/layout', component: () => import('@/layout'), redirect: '/home', children: [
-      { path: '/home', component: () => import('@/views/Home') },
-      { path: '/table', component: () => import('@/views/Table') },
-      { path: '/list', component: () => import('@/views/List') },
+    path: '/layout', component: () => import('@/layout/index.js'), children: [
+      { path: '/layout', name: 'layout', component: () => import('@/views') },
+      // 通用
+      { path: '/general/button', name: 'button', component: () => import('@/views/general/Button') },
+      { path: '/general/icon', name: 'icon', component: () => import('@/views/general/Icon') },
       // 导航
-      {
-        path: '/navigation', component: () => import('@/views/navigation'), children: [
-          { path: '/navigation/affix', component: () => import('@/views/navigation/Affix') },
-          { path: '/navigation/breadcrumb', component: () => import('@/views/navigation/Breadcrumb') },
-          { path: '/navigation/dropdown', component: () => import('@/views/navigation/Dropdown') },
-          { path: '/navigation/menu', component: () => import('@/views/navigation/Menu.js') },
-        ]
-      },
+      { path: '/navigation/affix', name: 'affix', component: () => import('@/views/navigation/Affix') },
+      { path: '/navigation/breadcrumb', name: 'breadcrumb', component: () => import('@/views/navigation/Breadcrumb') },
+      { path: '/navigation/dropdown', name: 'dropdown', component: () => import('@/views/navigation/Dropdown') },
+      { path: '/navigation/menu', name: 'menu', component: () => import('@/views/navigation/Menu.js') },
+      // 数据输入
+      { path: '/dataEntry/circle', name: 'circle', component: () => import('@/views/dataEntry/Circle') },
+      // 数据显示
+      { path: '/dataDisplay/list', name: 'list', component: () => import('@/views/dataDisplay/List') },
+      { path: '/dataDisplay/table', name: 'table', component: () => import('@/views/dataDisplay/Table') },
+      // 反馈
+
+      // 其他
+
+      // 插件
+
+      // 待开放
 
     ]
   },
-  { path: '/Unusual', component: () => import('@/views/Unusual') },
+  { path: '/Unusual', name: 'Unusual', component: () => import('@/views/Unusual') },
   { path: '*', component: () => import('@/views/Error') }
 ]
 
